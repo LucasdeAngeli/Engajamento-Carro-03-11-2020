973f0bda2ebd97618c7654752325a637abcef308
Lucas de Angeli <lucasdeangeli3@gmail.com>
2020-11-04 00:57:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+class MainClass {
+  public static void Main (string[] args) {
+    string continuar="S", marca;
+    double consumo;
+    Carro aut = new Carro("TESTE", 0);
+    List<Carro> listaCarros = new List<Carro>();
+
+    while (continuar=="S"){
+      try {
+      Console.WriteLine("MARCA DO CARRO >>");
+      marca = Console.ReadLine();
+      Console.WriteLine("KM/L DO CARRO >>");
+      consumo = double.Parse(Console.ReadLine());
+      }
+      catch (FormatException){
+        marca = "PADRÃO";
+      }
+
+      bool ok=false;
+
+      while(!ok){
+        try{
+            aut = new Carro(marca, consumo);
+            ok=true;
+            Console.WriteLine("Lançado");
+            listaCarros.Add(aut);
+        }
+        catch (FormatException){
+          Console.WriteLine("Erro de Construtores")
+        }
+        finally {
+          Console.WriteLine("Deseja continuar? S/N");
+          continuar = Console.ReadLine();
+        }
+      }
+    }
+
+    if(listaCarros.Count==0){
+      throw new System.ArgumentExeption("A lista não pode estar vazia!");
+    }
+
+    Carro menor_consumo = listaCarros[0];
+
+    for(int i=1;i<listaCarros.Count;i++){
+      if(listaCarros[i].getConsumo() > menor_consumo.getConsumo())
+        menor_consumo = listaCarros[i];
+    }
+    Console.WriteLine("MARCA DE MENOR CONSUMO: {0} \n CONSUMO DE: {1} KM/L", menor_consumo.getMarca(), menor_consumo.getConsumo());
+
+    for(int i=0;i<listaCarros.Count;i++){
+      Console.WriteLine("-------------{0}------------", listaCarros[i].getMarca());
+      Console.WriteLine("LITROS GASTOS EM 1000 KM: {0}", 1000/listaCarros[i].getConsumo());
+      Console.WriteLine("VALOR GASTO AO PERCORRER 1000 KM: R$ {0}", (1000/listaCarros[i].getConsumo())*4.89);    
+    }
+
+  }
+}
